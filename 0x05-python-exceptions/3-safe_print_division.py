403edfbbc2dcc7789ac8ab8097f18c7fdd2aa2c9
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2022-12-19 21:46:15 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,15 @@
-Didi
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    """
+    a function that divides 2 integers
+    and prints the result.
+    """
+    try:
+        num_div = a / b
+    except (TypeError, ZeroDivisionError):
+        num_div = None
+    finally:
+        print("Inside result: {}".format(num_div))
+    return num_div
