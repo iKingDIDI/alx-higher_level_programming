d7a5fcb4a10adff82c3d81bce43fc92146eec983
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2022-12-19 21:36:29 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,13 @@
-Didi
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """
+    A function that prints an integer with
+    "{:d}".format()
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
