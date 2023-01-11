c79c695db12e4599d5183245c267767d180ecfee
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-01-11 15:59:49 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
-Didi
+#!/usr/bin/python3
+''' function that returns a python object represented by a JSON string
+'''
+
+import json
+
+
+def from_json_string(my_str):
+    ''' module from_json_string
+     returns Python objects
+    '''
+    return json.loads(my_str)
