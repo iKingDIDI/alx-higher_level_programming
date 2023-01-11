cc95df0facd344f9f23cccf127bbf8a0ebb4525a
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-01-11 16:02:11 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
-Didi
+#!/usr/bin/python3
+'''function that returns the dictionary description with simple data structure
+'''
+
+
+def class_to_json(obj):
+    '''module class_to_json
+       returns builds a dictionary
+    '''
+    return obj.__dict__
