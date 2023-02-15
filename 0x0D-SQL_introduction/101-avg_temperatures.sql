977bec56263063d8eeed2e399675f30a2a7c3ea3
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-02-15 09:55:43 +0100

    Update 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,1 +1,6 @@
-Didi
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
