0ebe140aba2cfe4a4c66c74462ad921314fb18fb
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-02-15 10:02:22 +0100

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,6 @@
-Didi
+-- Lists all records of the table second_table having a name value.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
