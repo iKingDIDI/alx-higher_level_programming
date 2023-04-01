21a3b2aae32018b1e591abe94e807d43590dbd82
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-01 16:07:58 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL get body size
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Sends a request to that URL, and displays the size of the body of the response
+curl -s "${1}" | wc -c
