869dc20a67dcfdfd5d11bf1c0dc99fde8dbafcba
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-01 16:11:37 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a DELETE request
-curl -s "$1" -X DELETE
+# Sends a DELETE request to the URL passed as the first argument and displays the body of the respons
+curl -s -X DELETE "${1}"
