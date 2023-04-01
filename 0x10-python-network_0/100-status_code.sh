9d0fb96330171cb75b8bc795b43c27e7dd9e027e
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-01 16:09:42 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request and only show the status code
-curl -s -o /dev/null -I --w "%{http_code}" "$1"
+# Script that sends a request to a URL passed as an argument, and displays only the status code of the response
+curl -so /dev/null -w "%{http_code}" "$1"
