524b2532c7c7e2ba15465cbc35767c2409f18459
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-21 10:20:51 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,15 @@
-Didi
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = () => myObject.value++;
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
