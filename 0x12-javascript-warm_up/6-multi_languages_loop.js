34f8cb47795cf825200a53b91c8950ed18d3b684
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-21 10:34:40 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,7 @@
-Didi
+#!/usr/bin/node
+
+// ['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach((e) => console.log(e));
+
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+msg.forEach((element) => console.log(element));
