c54bd5347a6d814bba39c7a429296cb3db3f2e78
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-25 15:31:22 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,6 @@
-Didi
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
