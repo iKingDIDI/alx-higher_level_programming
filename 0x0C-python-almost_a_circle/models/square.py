15b6486b98d53f15b538502639c349447f2f769b
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-26 11:46:13 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,58 +1,64 @@
 #!/usr/bin/python3
-"""The square"""
-from models.base import Base
+"""Define Rectangle Class
+"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A square class inheriting from rectangle"""
+    """Module Representation of Square
+"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Constructs the square's attributes"""
+        """Initialization a Square
+        """
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """String method for rectangle class"""
-        str_res = ("[Square] ({}) {}/{} - {}"
-                   .format(self.id, self.x, self.y, self.width))
-        return str_res
-
-    def area(self):
-        """Returns the area of the square"""
-        return self.width ** 2
 
     @property
     def size(self):
-        """Gets the"""
+        """module Square size getter
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """Sets the size of square"""
+        """module Square size setter
+        """
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """Updates the public class
-        Args:
-            *args(any): the list of arguments - no-keyworded arguments
-            **kwargs(any):
+    def __str__(self):
+        """module string represation of square
         """
-        if not args and not kwargs:
-            return
-        if args:
-            attributes = ["id", "size", "x", "y"]
-            for i, j in enumerate(args):
-                if i < len(attributes):
-                    setattr(self, attributes[i], j)
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
+
+    def update(self, *args, **kwargs):
+        """module update square
+        """
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
         else:
-            for k, v in kwargs.items():
-                if hasattr(self, k):
-                    setattr(self, k, v)
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """Converts to dictionary"""
-        _map = super().to_dictionary()
-        _map["size"] = _map["width"]
-        del _map["width"], _map["height"]
-        return _map
+        """retrun dictonary
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
