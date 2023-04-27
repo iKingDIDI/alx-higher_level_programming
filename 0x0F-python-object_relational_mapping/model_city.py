7aaba4b819e13a89e2869cda730871d5cf347864
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-27 11:11:29 +0100

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-'''task 14 model script'''
+"""
+Contains the class definition of a City
+"""
 
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
-from sqlalchemy import Column, String, Integer, ForeignKey
 
 
 class City(Base):
-    '''City model for my db'''
+    """
+    Representation of the table cities
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False,
-                autoincrement=True, primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
