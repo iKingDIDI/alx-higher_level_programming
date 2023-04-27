a22e7779c4ea397350e8ef672e18a11f1fe08537
iKingDIDI <106996755+iKingDIDI@users.noreply.github.com>
2023-04-27 13:00:52 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
-Didi
+#!/usr/bin/node
+
+const esrever = (list) => {
+  const reversedArray = [];
+  list.forEach((element) => reversedArray.unshift(element));
+  return (reversedArray);
+};
+
+module.exports = { esrever };
